4de47aebdfca5279c9bd1162c59b0844ef91b716
Hosein Alirezaee <hoseinalirezaee128@gmail.com>
2020-10-15 13:29:40 +0330

    add checks to see if a file is in PDF format

--- pipelines.py
+++ pipelines.py
@@ -1,25 +1,6 @@
-import json
 import os
 
 import signals
-
-
-class SavePipeline:
-    save_path = None
-    file = None
-
-    def open_spider(self, spider):
-        self.save_path = getattr(spider, 'save_path', os.getcwd())
-        if os.path.isdir(self.save_path):
-            self.save_path = os.path.join(self.save_path, 'articles.json')
-        self.file = open(self.save_path, 'wt')
-
-    def process_item(self, item, spider):
-        self.file.write('%s\n' % json.dumps(item, ensure_ascii=False))
-        return item
-
-    def close_spider(self, spider):
-        self.file.close()
 
 
 class SaveFilePipeline:
@@ -34,10 +15,15 @@ class SaveFilePipeline:
     def process_item(self, item, spider):
         file = item.pop('file')
         file_name = item['file_name']
-        with open(os.path.join(self.save_dir, file_name), 'wb') as f:
-            f.write(file)
+        is_pdf = False
+        if file.startswith(b'%PDF'):
+            is_pdf = True
+            with open(os.path.join(self.save_dir, file_name), 'wb') as f:
+                f.write(file)
+
         spider.crawler.signals.send_catch_log(signal=signals.file_downloaded,
                                               file_name=file_name,
                                               total_count=spider.articles_count,
-                                              downloaded_count=spider.downloaded_count)
+                                              downloaded_count=spider.downloaded_count,
+                                              is_pdf=is_pdf)
         return item
